91415f792768cf2072fe82ae4fd9b90738f7b328
TIMM SNOW <two.tone.snow@gmail.com>
2021-02-19 11:28:08 -0500

    completes 'skills' spreadsheet number 3, method practice

---
+++ skills.rb
@@ -0,0 +1,41 @@
+# puts "please enter a number"
+# number1 = gets.chomp.to_i
+# puts "please enter another number"
+# number2 = gets.chomp.to_i
+# puts number1 + number2
+
+# puts "please enter a word"
+# word = gets.chomp
+# puts word.reverse
+
+# puts "please enter a number"
+# number = gets.chomp.to_i
+# puts number * 10
+
+# puts "please enter a word"
+# word = gets.chomp.upcase
+# puts "please eneter another word"
+# word2 = gets.chomp.upcase
+# puts word + " " + word2
+
+# puts "please enter a word"
+# word = gets.chomp
+
+# puts word.length
+
+# puts "please enter a number"
+# number = gets.chomp.to_i
+# if number < 0
+#   puts "thats a negative number"
+# end
+
+# puts "please enter two numbers"
+# number = gets.chomp.to_i
+# number2 = gets.chomp.to_i
+# puts number * number2
+
+puts "please write a word"
+word = gets.chomp
+if word.length > 5
+  puts "thats a long word!"
+end
